b2d9845fc5e6e23a9c7e0d401bfb0879663bc5fd
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-12-11 16:45:00 +0900

    fix class component

--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function Hello(props) {
-  return <div>こんにちは {props.name} さん！！</div>
+// 関数コンポーネント
+// export default function Hello(props) {
+//   return <div>こんにちは {props.name} さん！！</div>
+// }
+
+// クラスコンポーネント
+export default class Hello extends React.Component {
+  render() {
+    return <div>こんにちは {this.props.name} さん！！<br />
+    これはクラスコンポーネントです</div>
+  }
 }
