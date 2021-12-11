2d2ea632c61da59744a1bcfa544c8ddf5cc499c7
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-12-11 16:23:08 +0900

    add Hello.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Hello name="山田" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hello(props) {
+  return <div>こんにちは {props.name} さん！！</div>
+}
